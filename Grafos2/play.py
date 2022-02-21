2a19fae5d8cad0afecf5d94468c45080deae63ec
Eduardo Gurgel <51385738+EduardoGurgel@users.noreply.github.com>
2022-02-21 20:28:32 -0300

    Update play.py

--- Grafos2/play.py
+++ Grafos2/play.py
@@ -3,98 +3,170 @@ from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 from pathfinding.core.diagonal_movement import DiagonalMovement
 
-pygame.init()
-screen = pygame.display.set_mode((1280,736))
-clock = pygame.time.Clock()
-class caminho:
+class Pathfinder:
 	def __init__(self,matriz):
 
-		# base
+		# inicial
 		self.matriz = matriz
 		self.grid = Grid(matrix = matriz)
-		self.select_surf = pygame.image.load('img/mouse.png').convert_alpha()
+		self.modelo_icone = pygame.image.load('img\selection.png').convert_alpha()
 
-		# caminho
+		# busca do caminho
 		self.caminho = []
-	def ponto_ativo(self):
-		mouse_pos = pygame.mouse.get_pos()
-		row =  mouse_pos[1] // 32 # Posição exata do mouse na linha
-		col =  mouse_pos[0] // 32 # Posição exata do mouse na coluna
-		celula_atual = self.matrix[fila][col]
-		if celula_atual == 1:
-			rect = pygame.Rect((col * 32,fila * 32),(32,32))
-			screen.blit(self.select_surf,rect)
-	
-	def cria_rastro(self):
 
-		# inicio
-		inicio_x, inicio_y = self.roomba.sprite.get_coord()
+		# Roomba
+		self.roomba = pygame.sprite.GroupSingle(Roomba(self.caminho_vazio))
+
+	def caminho_vazio(self):
+		self.caminho = []
+
+	def mostra_rastro(self):
+		mouse_pos = pygame.mouse.get_pos()
+		fila =  mouse_pos[1] // 32 # Posição exata do mouse na linha
+		col =  mouse_pos[0] // 32 # Posição exata do mouse na coluna
+		valor_atual = self.matriz[fila][col]
+		if valor_atual == 1:
+			rect = pygame.Rect((col * 32,fila * 32),(32,32))
+			screen.blit(self.modelo_icone,rect)
+
+	def cria_caminho(self):
+
+		# início
+		inicio_x, inicio_y = self.roomba.sprite.get_coordenada()
 		inicio = self.grid.node(inicio_x,inicio_y)
 
-		# fim
+		# Fim
 		mouse_pos = pygame.mouse.get_pos()
 		fim_x,fim_y =  mouse_pos[0] // 32, mouse_pos[1] // 32
 		fim = self.grid.node(fim_x,fim_y)
 
 		# o caminho é recalculado a cada click
-		finder = AStarFinder(DiagonalMovement = DiagonalMovement.always)
+		finder = AStarFinder(diagonal_movement = DiagonalMovement.always)
 		self.caminho,_ = finder.find_path(inicio,fim,self.grid)
 		self.grid.cleanup()
-		
-class Game(pygame.sprite.Sprite):
-	def _init_(self,sem_caminho):
+		self.roomba.sprite.define_caminho(self.caminho)
+
+	def desenha_rastro(self): # Mostra o caminho a ser percorrido
+		if self.caminho:
+			points = []
+			for point in self.caminho:
+				x = (point[0] * 32) + 16
+				y = (point[1] * 32) + 16
+				points.append((x,y))
+
+			pygame.draw.lines(screen,'#4a4a4a',False,points,5)
+
+	def update(self):
+		self.mostra_rastro()
+		self.desenha_rastro()
+
+		# roomba updating and drawing
+		self.roomba.update()
+		self.roomba.draw(screen)
+
+class Roomba(pygame.sprite.Sprite):
+	def __init__(self,caminho_vazio):
 
 		# base
-		super()._init_()
-		self.image = pygame.image.load('img/roomba.png').convert_alpha()
-		self.retangulo = self.image.get_retangulo(center = (60,60))
+		super().__init__()
+		self.image = pygame.image.load('img/boneco.png').convert_alpha()
+		self.rect = self.image.get_rect(center = (60,60))
 
 		# movimento
-		self.posicao = self.retangulo.center
+		self.pos = self.rect.center
 		self.velocidade = 3
-		self.direcao = pygame.math.Vector2(0,0)
+		self.direction = pygame.math.Vector2(0,0)
 
 		# caminho
 		self.caminho = []
-		self.retangulo_colisao = []
-		self.sem_caminho = empty_path
+		self.collision_rects = []
+		self.caminho_vazio = caminho_vazio
 
-	def get_coord(self): #Transforma a posição atual em uma coordenada
-		col = self.retangulo.centerx // 32
-		row = self.retangulo.centery // 32
+	def get_coordenada(self): #Transforma a posição atual em uma coordenada
+		col = self.rect.centerx // 32
+		fila = self.rect.centery // 32
 		return (col,fila)
 
-	def set_path(self,caminho):
+	def define_caminho(self,caminho):
 		self.caminho = caminho
-		self.cria_colisao_retangulo()
+		self.create_collision_rects()
 		self.get_direcao()
 
-	def cria_colisao_retangulo(self): # Identificando colisão no mapa
+	def create_collision_rects(self): # Identificando colisão no mapa
 		if self.caminho:
-			self.colisao_retangulo = []
-			for point in self.path:
+			self.collision_rects = []
+			for point in self.caminho:
 				x = (point[0] * 32) + 16
 				y = (point[1] * 32) + 16
-				retangulo = pygame.Rect((x - 2,y - 2),(4,4))
-				self.collision_rects.append(retangulo)
+				rect = pygame.Rect((x - 2,y - 2),(4,4))
+				self.collision_rects.append(rect)
 
-	def get_direction(self):
+	def get_direcao(self):
 		if self.collision_rects:
 			inicio = pygame.math.Vector2(self.pos)
 			fim = pygame.math.Vector2(self.collision_rects[0].center)
-			self.direcao = (fim - inicio).normalize()
+			self.direction = (fim - inicio).normalize()
 		else:
-			self.direcao = pygame.math.Vector2(0,0)
+			self.direction = pygame.math.Vector2(0,0)
 			self.caminho = []
 
-bg_surf = pygame.image.load('img/map.jpg').convert()
+	def check_collisions(self):
+		if self.collision_rects:
+			for rect in self.collision_rects:
+				if rect.collidepoint(self.pos):
+					del self.collision_rects[0] # Remove o caminho quando alcança o objetivo
+					self.get_direcao()
+		else:
+			self.caminho_vazio()
+
+	def update(self):
+		self.pos += self.direction * self.velocidade
+		self.check_collisions()
+		self.rect.center = self.pos
+
+# base inicial
+pygame.init()
+screen = pygame.display.set_mode((1280,736))
+clock = pygame.time.Clock()
+
+# mapa
+bg_surf = pygame.image.load('img\map.png').convert()
+matriz = [
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,1,1,1,1,1,0,0,0,0,1,1,1,1,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,1,1,1,1,1,0,0,0,0,1,1,1,1,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0],
+	[0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1,1,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1,1,0,0,1,0,0,1,1,1,1,0,0,0,0,0,0,1,1,1,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,0,0,0,0,1,1,1,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0],
+	[0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0],
+	[0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+	[0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+	[0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0],
+	[0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,1,1,1,0,0,0],
+	[0,1,1,1,1,1,0,0,1,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,1,1,1,1,0,0,0],
+	[0,0,0,1,1,1,0,0,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,0],
+	[0,0,0,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,0],
+	[0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,0,0,1,1,0],
+	[0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,0,0,1,1,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+pathfinder = Pathfinder(matriz)
 
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
-	screen.blit(bg_surf, (0, 0))
+		if event.type == pygame.MOUSEBUTTONDOWN: # Cria o caminho a cada click
+			pathfinder.cria_caminho()
+
+	screen.blit(bg_surf,(0,0))
+	pathfinder.update()
+
 	pygame.display.update()
 	clock.tick(60)
-
